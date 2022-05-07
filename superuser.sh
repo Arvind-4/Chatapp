4d5fc9d230dae90c91ffe040c86cc4e6dd7cff81
Arvind-4 <lordarvindthegamer@gmail.com>
2022-05-07 20:57:51 +0530

    Updated: removed unused files

--- superuser.sh
+++
@@ -1,7 +0,0 @@
-echo "Create a Super User"
-python manage.py createsuperuser
-echo "Successfully Created Superuser"
-echo "Opening Web Browser ..."
-python -m webbrowser http://localhost:8000/
-echo "Now Running Python server"
-python manage.py runserver
