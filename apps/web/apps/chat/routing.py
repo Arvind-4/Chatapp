1a3e8ad9e72e9ade1c74837bea012abf45b999b8
Arvind-4 <lordarvindthegamer@gmail.com>
2023-01-06 22:56:00 +0530

    Updated: routing.py in chat

--- apps/web/apps/chat/routing.py
+++ apps/web/apps/chat/routing.py
@@ -1,8 +1,16 @@
 from django.urls import re_path
+from django.conf import settings
 
 from .consumers import ChatConsumer
 
-websockets_urlpatterns = [
-    re_path(r'^ws/(?P<room_name>[^/]+)/', ChatConsumer.as_asgi()), 
-    re_path(r'^wss/(?P<room_name>[^/]+)/', ChatConsumer.as_asgi()), 
-]
+websockets_urlpatterns = []
+
+if settings.DEBUG:
+    websockets_urlpatterns += [
+        re_path(r'^ws/(?P<room_name>[^/]+)/', ChatConsumer.as_asgi()), 
+    ]
+
+else:
+    websockets_urlpatterns += [
+        re_path(r'^wss/(?P<room_name>[^/]+)/', ChatConsumer.as_asgi()), 
+    ]
