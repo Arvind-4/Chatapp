f357768ef40ba9cfbabbfe4ed60f752eefa3a90f
Arvind-4 <lordarvindthegamer@gmail.com>
2023-01-06 19:26:46 +0530

    Updated: settings in __init__.py

--- apps/web/backend/settings/__init__.py
+++ apps/web/backend/settings/__init__.py
@@ -1,1 +1,8 @@
-from .local import *
+from decouple import config
+
+DJANGO_LIVE = config('DJANGO_LIVE', default=False, cast=bool)
+
+if DJANGO_LIVE:
+    from .local import *
+else:
+    from .production import *
