3bb6cf66a343163a5f4354382bab58d464a7d31b
Arvind-4 <lordarvindthegamer@gmail.com>
2023-01-12 12:13:05 +0530

    Updated: build.sh script

--- build.sh
+++ build.sh
@@ -2,17 +2,13 @@
 
 set -o errexit
 
-echo "Upgrade pip..."
 python -m pip install --upgrade pip
 
-echo "Installing dependencies..."
 python -m pip install -r requirements.txt
 
-echo "Migrating database..."
 python manage.py makemigrations --noinput
 python manage.py migrate --noinput
 
-echo "Creating superuser..."
 
 DJANGO_SUPERUSER_EMAIL=${DJANGO_SUPERUSER_EMAIL}
 DJANGO_SUPERUSER_USERNAME=${DJANGO_SUPERUSER_USERNAME}
@@ -21,15 +17,6 @@ DJANGO_SUPERUSER_PASSWORD=${DJANGO_SUPERUSER_PASSWORD}
 python manage.py createsuperuser \
     --email $DJANGO_SUPERUSER_EMAIL \
     --noinput || true
-
-echo "Running npm install..."
-npm run i
-
-echo "Running npm Production..."
-npm run production
-
-echo "Clear Cache..."
-rm -rf apps/chatjs
 
 echo "Collecting static files..."
 python manage.py collectstatic --noinput --clear
